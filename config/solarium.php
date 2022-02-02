a1f54633960b6546aeff57bc69d80c03664cad6a
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2022-02-02 11:04:28 +0200

    Update solarium.php

--- config/solarium.php
+++ config/solarium.php
@@ -3,7 +3,7 @@
 return [
     'endpoint' => [
         'localhost' => [
-            'host' => env('SOLR_HOST', '127.0.0.1'),
+            'host' => env('SOLR_HOST', 'awtar-tech.com'),
             'port' => env('SOLR_PORT', '8983'),
             'path' => env('SOLR_PATH', '/solr/'),
             'core' => env('SOLR_CORE', 'collection1')
