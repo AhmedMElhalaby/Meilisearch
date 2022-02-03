7e49567ae269cd48096801b947d38e6bf581221b
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2022-02-03 19:48:58 +0200

    .

--- app/Http/Controllers/MeiliesearchController.php
+++ app/Http/Controllers/MeiliesearchController.php
@@ -10,7 +10,7 @@ class MeiliesearchController extends Controller
 {
     public function store_doc()
     {
-        $client = new Client('http://127.0.0.1:7700', 'masterKey');
+        $client = new Client('http://127.0.0.1:7700', 'hv7SEHtabef5f8dea8218486c50bf7d10eb467e1a4d58895d6d7dc121bec7c173a9a4e9c');
         $index = $client->index('images');
         $index->addDocuments(Image::with('tags')->get());
         return 'Done!';
